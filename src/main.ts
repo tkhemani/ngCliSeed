76532ce752832161a89befc37eec09ba984395b7
Tarun Khemani <Tarun.Khemani@morganstanley.com>
2016-06-17 08:38:47 -0400

    working basic app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  // Initialize Firebase app  
+  defaultFirebase({
+    apiKey: "uuiXDBcfoae3cuvuLGBTRXQk2ex6WBYzgkqIcvEU",
+    authDomain: null,
+    databaseURL: "https://hatplay.firebaseio.com",
+    storageBucket: null,
+  })
+]);
 
